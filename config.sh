cf6cafac7af81359355d096f00d4ac4deacf6c77
Peter Bortas <bortas@gmail.com>
2017-08-07 23:01:17 +0200

    Document and check for wpull workaround

--- config.sh
+++ config.sh
@@ -13,15 +13,27 @@ MYPIPELINE=YOUR-PIPELINE-NAME-GOES-HERE
 # Uncomment this if you only want to handle !ao/!archiveonly jobs
 #export AO_ONLY=1
 
-# Make sure whatever is needed is in the path
-export PATH=$PATH:/opt/ateam/bin/:$HOME/ArchiveBot/pipeline/archivebot/
+# Make sure whatever is needed* is in the path
+# * phantomjs and wpull as of writing
+export PATH=$PATH:/opt/ateam/bin/:$HOME/.local/bin/wpull
 
+# FIXME: The above wont really work for wpull right now. The pipeline is
+# throwing away PATH somewhere, so wpull needs to be in the default
+# PATH: /usr/bin/wpull
 
 ######## Do not edit anything below this line ########
 
 export RSYNC_URL=rsync://fos.textfiles.com/archivebot/
 export REDIS_URL=redis://127.0.0.1:16379/0
 export FINISHED_WARCS_DIR=$HOME/warcs4fos
+
+# FIXME: This is abhorrent
+if [ ! -x /usr/bin/wpull ]; then
+    echo "FATAL: For currently unknown reasons the pipeline needs wpull to be in /usr/bin/wpull"
+    echo "The current workaround is to link is there from the pip installation:"
+    echo "ln -s ~archivebot/.local/bin/wpull /usr/bin/wpull"
+    exit 1
+fi
 
 if [ $MYUSER = "YOUR-USERNAME-GOES-HERE" ]; then
     echo "FATAL: No account configured, unable to start tunnel"
